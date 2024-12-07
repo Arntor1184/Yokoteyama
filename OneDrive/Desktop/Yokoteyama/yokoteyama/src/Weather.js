c0fb61f6ae5db6ad580411eaf47d281f95c4504a
Arntor1184 <hammerofmelkor@gmail.com>
2024-12-07 14:55:17 +1100

    All components added but with no styling

---
+++ OneDrive/Desktop/Yokoteyama/yokoteyama/src/Weather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Weather() {
+  return (
+    <div>
+      <h1>Weather Dashboard</h1>
+      <p>Hello Sacchi and Nyappymokkomokkosama</p>
+    </div>
+  );
+}
+
+export default Weather;
